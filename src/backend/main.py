9a6b68d896d43f1748aaa95b7524c58484d10c2c
slocksert <rsdlj01@gmail.com>
2025-06-10 01:00:56 -0300

    changing origins

--- src/backend/main.py
+++ src/backend/main.py
@@ -34,7 +34,7 @@ async def startup_event():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.allow_origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
